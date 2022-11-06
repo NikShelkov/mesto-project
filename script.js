d979245d81064eff6dbfa7e285b6c22826bd8068
Nikita Shelkov <nikita.shelkov1983@yandex.ru>
2022-11-06 16:41:58 +0400

    js code for edit button cradted

---
+++ script.js
@@ -0,0 +1,22 @@
+/*let body = document.querySelector('.page'); */
+let editButton = document.querySelector('.profile__edit-button');
+let popupClosed = document.querySelector('.popup');
+let popupOpened = document.querySelector('.popup_opened');
+
+function popupVisible () {
+    let editButton = document.querySelector('.profile__edit-button');
+
+    editButton.addEventListener ('click', popupVisible); 
+    popupClosed.classList.add('popup_opened'); 
+}
+
+popupVisible ();
+
+function popupInvisible () {
+    let closeButton = document.querySelector ('.popup__close-icon')
+
+    closeButton.addEventListener ('click', popupInvisible);
+    popupClosed.classList.remove('popup_opened');
+}
+
+popupInvisible (); 
